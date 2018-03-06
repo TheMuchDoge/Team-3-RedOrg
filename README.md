f63565fc1fabb564f1eee1b4998055c57cfa7179
Noorsesverre <noorsesverre@live.nl>
2018-03-06 15:35:01 +0100

    Commit #2
    Trying to get a connected version

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-School project for IDRI1005.
-Team 3, called RedOrg, short for Red Organizator.
-program made for The Red Cross, to keep track of events and who will be there to represent The Red Cross
+# electron-react-example
--- src/services.js
+++ src/services.js
@@ -4,16 +4,16 @@ import mysql from 'mysql';
 let connection;
 function connect() {
   connection = mysql.createConnection({
-    host     : 'mysql.stud.iie.ntnu.no',
-    user     : 'g_oops_3',
-    password : 'rb8BFX1X',
-    database : 'g_oops_3'
+    host: 'mysql.stud.iie.ntnu.no',
+    user: 'g_oops_3',
+    password: 'rb8BFX1X',
+    database: 'g_oops_3'
   });
 
   // Connect to MySQL-server
   connection.connect((error) => {
     if (error) throw error; // If error, show error in console and return from this function
-    console.log('We are connected!');
+    console.log('We are connected')
   });
 
   // Add connection error handler
@@ -28,12 +28,6 @@ function connect() {
 }
 connect();
 
-class RedOrgBrukere {
-  finnBruker(callback) {
-    connection.query('SELECT * FROM bruker WHERE id=?', (error, result) => {
-      if (error) throw error;
+// Class that performs database queries related to customers
 
-      callback(result[0]);
-    });
-  }
-}
+export {/*class naem here */}
