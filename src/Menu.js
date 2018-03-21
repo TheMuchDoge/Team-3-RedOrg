ed07e1fd09a2795aeff33805a4e69d10e906ab7f
TheMuchDoge <32930046+TheMuchDoge@users.noreply.github.com>
2018-03-21 13:35:11 +0100

    signout is not working

--- src/Menu.js
+++ src/Menu.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
 import {queries} from './services.js';
-
+import createHashHistory from 'history/createHashHistory';
+const history = createHashHistory();
 
 class Menu extends React.Component {
     constructor(props) {
@@ -9,30 +10,43 @@ class Menu extends React.Component {
     }
 
     render() {
-        let brukerLoggetInn = queries.brukerLoggetInn();
-        if (brukerLoggetInn) {
-            return (
-                <div>
-                    <NavLink activeStyle={{color: 'green'}} exact to='/home'>Home</NavLink>{' '}
-                    <NavLink activeStyle={{color: 'green'}} exact to='/kalender'>Kalender</NavLink>{' '}
-                    <NavLink activeStyle={{color: 'green'}} exact to='/profile'>Profile</NavLink>{' '}
-                    <NavLink activestyle={{color: 'green'}} exact to="/login">Sign out</NavLink>
-                </div>
 
-            );
-        }
-        return (
-            <div>
-                <NavLink activeStyle={{color: 'green'}} exact to='/login'>Logg Inn</NavLink>{' '}
-                <NavLink activeStyle={{color: 'green'}} exact to='/signUp'>Registrer</NavLink>{' '}
-            </div>
-        );
+            let brukerLoggetInn = queries.brukerLoggetInn();
+            if (brukerLoggetInn) {
+                return (
+                    <div>
+                        <NavLink activeStyle={{color: 'green'}} to='/home'>Home</NavLink>{' '}
+                        <NavLink activeStyle={{color: 'green'}} to='/kalender'>Kalender</NavLink>{' '}
+                        <NavLink activeStyle={{color: 'green'}} to='/profile'>Profile</NavLink>{' '}
+                        <span>
+                        <input type="text" placeholder="Søk" ref="searchInput"/><button ref="searchButton"><NavLink activeStyle={{color: 'green'}} to='/searchResult'>Søk</NavLink></button>
+                    </span>
+                        <NavLink activestyle={{color: 'green'}} exact to="/">Sign out</NavLink>
+                    </div>
+                );
+            }
+            else {
+                return(
+                <div>
+                    <NavLink activestyle={{color: 'green'}} exact to="/login">Logg inn</NavLink>
+                    <NavLink activestyle={{color: 'green'}} exact to="/signup">Registrer</NavLink>
+                </div>)
+            }
+
 
     }
+
+    componentDidMount () {
+        this.ref.signout.onclick = () => {
+            localStorage.removeItem('loggetInnBruker');
+            history.push('/login')
+        }
+    }
+
 
 
 }
 
 
-export default Menu;
 
+export default Menu;
