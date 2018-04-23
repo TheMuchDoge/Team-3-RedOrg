30dd15af1460bec4e2570e4caa7d377d9114c9ec
Noorsesverre <noorsesverre@live.nl>
2018-04-23 17:49:55 +0200

    final commit

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const Menu = electron;
 
 let mainWindow;
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({width: 670, height: 800});
+  mainWindow = new BrowserWindow({width: 720, height: 800});
   mainWindow.loadURL('file://' + __dirname + '/index.html');
-
+  
 });
 
 app.on('window-all-closed', () => {
